93910d8c7aa84d817cc427e1c39210b1692334be
Martin Stam <martin.stam@home.nl>
2019-07-10 15:20:13 +0200

    Add files via upload

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+module Turbowin_jws {
+   requires transitive java.desktop;
+   requires transitive java.logging;
+   requires com.fazecast.jSerialComm;
+   requires java.jnlp;
+    
+}
